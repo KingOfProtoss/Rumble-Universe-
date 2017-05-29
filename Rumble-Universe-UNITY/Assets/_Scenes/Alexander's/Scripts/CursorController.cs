368958022be5748a9fbbc38c4dee111d2e9b7f31
AlexanderTingling <1tinglingale@hdsb.ca>
2017-05-29 18:35:52 -0400

    Added Sloth

---
+++ Rumble-Universe-UNITY/Assets/_Scenes/Alexander's/Scripts/CursorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour {
+
+	public Vector3 Sloth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 pz = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		pz.z = 0;
+		gameObject.transform.position = pz;
+		Sloth = gameObject.transform.position;
+			
+	}
+}
